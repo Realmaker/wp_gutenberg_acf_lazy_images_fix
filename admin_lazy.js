ae6a275054336f2addfd3c72c37823e29ee1faae
Realmaker <info@realmaker.de>
2019-07-17 19:10:52 +0200

    V1

---
+++ admin_lazy.js
@@ -0,0 +1,21 @@
+jQuery(window).load(function($) {
+
+	function show_lazy_images() {
+    jQuery('.wp-block img.lazy').each(function() {
+    	jQuery(this).attr('src',jQuery(this).attr('data-src'));
+    });
+	}
+
+	show_lazy_images();
+
+	setTimeout(
+	function() 
+	{
+	  show_lazy_images();
+	}, 5000);
+
+	jQuery('.edit-post-header__settings .editor-post-publish-button').click(function() {
+		show_lazy_images();
+	});
+
+});
